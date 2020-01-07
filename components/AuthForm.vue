1e24a103cc90c22e40f98cac2957b3befb062f47
Grigory Kazakov <grigory.kazakov21@gmail.com>
2020-01-07 16:01:54 +0500

    add auth page, add auth component

---
+++ components/AuthForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <div class="columns">
+      <div class="column is-4">
+        <b-field label="Name">
+          <b-input v-model="name"></b-input>
+        </b-field>
+        <b-field label="Password">
+          <b-input type="password" password-reveal></b-input>
+        </b-field>
+        <b-button type="is-success" expanded>Sign in</b-button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String
+    },
+    password: {
+      type: String
+    }
+  }
+}
+</script>
